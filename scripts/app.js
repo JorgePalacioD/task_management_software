7091aca7dd2737290bd53887c6a2f8f9d394658f
Juanjoxznp <juanjoxznp@gmail.com>
2023-12-04 06:34:53 -0500

    Se agrega campo de asignacion de tareas

--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,89 @@
-const containerTasks = document.querySelector('#sect-tasks')
-const frmTask = document.getElementById('frm-task')
 
-const title = document.getElementById('title-task')
-const textTask = document.getElementById('Por-hacer')
+const containerTasks = document.querySelector('#sect-tasks');
+const frmTask = document.getElementById('frm-task');
+const modal = document.getElementById('modal');
 
-frmTask.addEventListener('submit', createCard)
+const title = document.getElementById('title-task');
+const textTask = document.getElementById('Por-hacer');
+
+frmTask.addEventListener('submit', createCard);
 
 function createCard(event) {
+  event.preventDefault();
 
-  event.preventDefault()
-  console.log(event);
+  const card = document.createElement('article');
+  card.classList.add('card');
+  card.setAttribute('id', 'card');
 
-  const card = document.createElement('article')
-  card.classList.add('card')
-  card.setAttribute('id','card')
+  const headerCard = document.createElement('div');
+  headerCard.classList.add('header-card');
 
-  const headerCard = document.createElement('div')
-  headerCard.classList.add('header-card')
+  const closeCard = document.createElement('button');
+  const titleCard = document.createElement('h2');
+  titleCard.textContent = title.value;
+  closeCard.classList.add('close');
+  closeCard.id = 'close';
+  closeCard.textContent = 'x';
 
-  const closeCard = document.createElement('button')
-  const titleCard = document.createElement('h2')
-  titleCard.textContent = title.value
-  closeCard.classList.add('close')
-  closeCard.id = 'close'
-  closeCard.textContent = 'x'
+  const containerCard = document.createElement('div');
+  containerCard.classList.add('container-card');
 
-  const containerCard = document.createElement('div')
-  containerCard.classList.add('container-card')
+  const descriptionCard = document.createElement('p');
+  descriptionCard.textContent = textTask.value;
 
-  const descriptionCard = document.createElement('p')
-  descriptionCard.textContent = textTask.value
-  const footerCard = document.createElement('div')
-  const reference = document.createElement('a')
-  reference.setAttribute('href','')
-  reference.textContent = 'KeyCode Team'
+  const categoryLabel = document.createElement('p');
+  categoryLabel.textContent = 'Tarea asignada a: ';
+  categoryLabel.style.color = 'blue';  
+  categoryLabel.style.fontWeight = 'bold'; 
 
-  headerCard.appendChild(titleCard)
-  headerCard.appendChild(closeCard)
 
-  containerCard.appendChild(descriptionCard)
 
-  footerCard.appendChild(reference)
+  const categorySelect = document.getElementById('task-category');
+  const selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
 
-  card.appendChild(headerCard)
-  card.appendChild(containerCard)
-  card.appendChild(footerCard)
+  const categoryValue = document.createElement('span');
+  categoryValue.textContent = selectedCategory;
 
-  containerTasks.appendChild(card)
-  
+  categoryValue.classList.add('red-text');
+
+
+  const footerCard = document.createElement('div');
+  const reference = document.createElement('a');
+  reference.setAttribute('href', '');
+  reference.textContent = 'KeyCode Team';
+
+  headerCard.appendChild(titleCard);
+  headerCard.appendChild(closeCard);
+
+  containerCard.appendChild(descriptionCard);
+  containerCard.appendChild(categoryLabel);
+  containerCard.appendChild(categoryValue);
+
+  footerCard.appendChild(reference);
+
+  card.appendChild(headerCard);
+  card.appendChild(containerCard);
+  card.appendChild(footerCard);
+
+  containerTasks.appendChild(card);
+
   closeCard.addEventListener('click', () => {
-    card.style.display = 'none'
-  })  
+    card.style.display = 'none';
+  });
+
+  closeModal();
+
+  frmTask.reset();
 }
 
+function closeModal() {
+  if (modal) {
+    modal.close();
+  }
+}
 
-
+function openModal() {
+  if (modal) {
+    modal.showModal();
+  }
+}
